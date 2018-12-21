85cbec9eca2dd4190aaf95b23700c0dbf1922883
Igor Kim <igkim90@gmail.com>
2018-12-21 20:09:11 +0500

    add error processing and update version

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -4,11 +4,17 @@ import program from 'commander';
 import pageLoader from '..';
 
 program
-  .version('0.0.7')
+  .version('0.0.9')
   .description('Download web pages')
   .arguments('<url>')
   .option('--output [type]', 'Output folder')
   .action((url, options) => {
-    pageLoader(url, options);
+    pageLoader(url, options)
+      .then(() => {
+        console.log('Page download succesful!');
+      }).catch((e) => {
+        console.error(e.message);
+        process.exit(1);
+      });
   })
   .parse(process.argv);
