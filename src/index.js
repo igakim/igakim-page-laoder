55a39073b39a29383f23fe67f8d730ea989e24e6
Igor Kim <igkim90@gmail.com>
2018-12-21 16:16:11 +0500

    refactor some names and add debug log

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import { promises as fsp, createWriteStream } from 'fs';
 import url from 'url';
 import path from 'path';
 import cheerio from 'cheerio';
+import debug from 'debug';
 
-import { getResourceName, linkReplace, getDownloadLinks } from './utils';
+import { getResourceName, replaceLinks, getResourcesLinks } from './utils';
 
+const a = debug('page-loader');
 const downloadResource = (resource, directory) => {
   const { actualUrl, fileName } = resource;
-  axios({
+  return axios({
     method: 'get',
     url: actualUrl,
     responseType: 'stream',
-  }).then((res) => {
-    res.data.pipe(createWriteStream(path.join(directory, fileName)));
-  }).catch(e => console.log(`Couldn't load ${actualUrl} \n ${e}`));
+  })
+    .then((res) => {
+      res.data.pipe(createWriteStream(path.join(directory, fileName)));
+      a('resource dowloaded %s', path.join(directory, fileName));
+    })
+    .catch((e) => {
+      a('resource wasn\'n downloaded %s', path.join(directory, fileName));
+      throw new Error(`One or more files were not downloaded. Response status - ${e.response.status}`);
+    });
 };
 
 const pageLoader = (pageUrl, options = {}) => {
@@ -24,22 +32,35 @@ const pageLoader = (pageUrl, options = {}) => {
   const assetsDirectoryName = getResourceName(path.join(host, pathname), '_files'); // hexlet-io-courses_files
   const fullPathToHtml = path.resolve(directory, fileName);
   const fullPathToAssetsDirectory = path.resolve(directory, assetsDirectoryName);
-  let downloadLinks;
+  let resourcesLinks;
   return axios.get(pageUrl)
     .then((res) => {
       const html = res.data;
       const $ = cheerio.load(html);
-      downloadLinks = getDownloadLinks($, { host, protocol });
-      const newBody = linkReplace($, assetsDirectoryName);
+      resourcesLinks = getResourcesLinks($, { host, protocol });
+      const newBody = replaceLinks($, assetsDirectoryName);
       return fsp.writeFile(fullPathToHtml, newBody);
     })
+    .then(() => a('html file downloaded'))
     .then(() => fsp.mkdir(fullPathToAssetsDirectory))
+    .then(() => a('assets directory created'))
     .then(() => {
-      downloadLinks.forEach((el) => {
-        downloadResource(el, fullPathToAssetsDirectory);
-      });
+      const promises = resourcesLinks.map(el => downloadResource(el, fullPathToAssetsDirectory));
+      return Promise.all(promises);
     })
-    .catch(e => console.log(e));
+    .then(() => a('all resources processed and downlaoded'))
+    .catch((e) => {
+      a('all resources processed');
+      a(e);
+    });
 };
+// Я понял что нужно контролировать, но не знаю как реализовать здесь.
+// Я исхожу из следующей лоики - Один из ресурсов может быть не скачан, например 404
+// Но остальные все равно должны быть скачаны, поэтому Promise.all для загрузки не подойдет.
+// Поэтому нужно бросать все по отдельности и скачивать (map).
+// Но как внутри map сделать без остановки не понимаю.
+// Решил сложить все промисы в массив и вернуть их.
+// Но если хоть один промис в состоянии rejected,
+// то ни один then дальше не отработает.
 
 export default pageLoader;
